272cc94297d22d340f74a23abe44f322fd62350e
Ilya <ilya.lyalinov@gmail.com>
2020-11-21 04:38:42 +0300

    add multithreading

--- edgesDetection/VideoPlayer.cpp
+++ edgesDetection/VideoPlayer.cpp
@@ -26,44 +26,65 @@ void VideoPlayer::playVideo() {
     Mat frame, oldFrame;
     Mat mean(size, CV_32F, Scalar::all(0));
     int frameCounter = 0;
+    mutex mtx, mtx1;
+    thread tNew, tOld;
+    Timer t1, t2;
 
     while (true) {
-        cout << "New frame" << '\n';
-        newFrameCap >> frame;
-        if (frame.empty()) {
-            break;
-        }
-
-        Mat gray;
-        cvtColor(frame, gray, COLOR_BGR2GRAY);
-        gray.convertTo(gray, CV_32F, 1.0 / 255.0);        
-        mean = mean + gray;
-
-        cout << "Frame number: " << frameCounter << "\n";
+        Mat gray1;
         frameCounter++;
 
+        if (!tNew.joinable()) {
+            tNew = std::thread([&]() {
+                t1.saveTimePoint();
+                newFrameCap >> frame;
+                cv::cvtColor(frame, gray1, COLOR_BGR2GRAY);
+                gray1.convertTo(gray1, CV_32F, 1.0 / 255.0);
+                lock_guard<mutex> lock(mtx);
+                mean = mean + gray1;
+                t1.saveTimePoint();
+            });
+        }
+
         if (frameCounter >= framesLimit) {
-            oldFrameCap >> oldFrame;
-            if (oldFrame.empty()) {
-                break;
+            if (!tOld.joinable()) {
+                tOld = std::thread([&]() {
+                    t2.saveTimePoint();
+                    oldFrameCap >> oldFrame;
+                    Mat gray2;
+                    cvtColor(oldFrame, gray2, COLOR_BGR2GRAY);
+                    gray2.convertTo(gray2, CV_32F, 1.0 / 255.0);
+                    lock_guard<mutex> lock(mtx1);
+                    mean = mean - gray2;
+                    t2.saveTimePoint();
+                });
             }
-            
-            Mat gray1;
-            cvtColor(oldFrame, gray1, COLOR_BGR2GRAY);
-            gray1.convertTo(gray1, CV_32F, 1.0 / 255.0);            
-            mean = mean - gray1;
+
+            tNew.join();
+            tOld.join();
+
+            cout << "1 thread: ";
+            t1.printLastDuration();
+            cout << "2 thread: ";
+            t2.printLastDuration();
 
             Mat meanCV8U;
             mean.convertTo(meanCV8U, CV_8U, 255.0 * 1.0 / framesLimit);
             imshow("Mean frame", meanCV8U);
-            
+
             Mat outputImage;
             detector->detect(meanCV8U, outputImage);
-            imshow("Filtered mean frame", outputImage);
+            imshow("Filtered mean frame", outputImage); 
         }
-
+        else {
+            tNew.join();
+            t1.printLastDuration();
+        }
+        
         imshow("New frame", frame);
         waitKey(1);
+        t1.reset();
+        t2.reset();
     }
 
     newFrameCap.release();
