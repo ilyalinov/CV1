d8f7e03650f0bf253d4c5a46d98560d6447c48e5
Ilya <ilya.lyalinov@gmail.com>
2021-02-04 03:09:22 +0300

    refactor

--- overlapDetection/src/VideoPlayer.cpp
+++ overlapDetection/src/VideoPlayer.cpp
@@ -24,105 +24,137 @@ void VideoPlayer::playVideo() {
         return;
     }
 
-    const int framesLimit = 500;
+    VideoWriter v;
+    if (videoRecordingFlag) {
+        initializeVideoWriter(v, newFrameCap, compressionFactor);
+    }
 
-    int c = 4;
-    Size size((int)newFrameCap.get(CAP_PROP_FRAME_WIDTH), (int)newFrameCap.get(CAP_PROP_FRAME_HEIGHT));
-    Size outputSize((int)newFrameCap.get(CAP_PROP_FRAME_WIDTH) / c, (int)newFrameCap.get(CAP_PROP_FRAME_HEIGHT) / c);
-    int ex = static_cast<int>(newFrameCap.get(CAP_PROP_FOURCC));
-    int fps = static_cast<int>(newFrameCap.get(CAP_PROP_FPS));
-    string name = "E:\\Downloads\\dumps\\dft1_" + to_string(c) + ".avi";
-    VideoWriter v(name, ex, fps, outputSize, false);
-    Mat frame, oldFrame;
-    Mat mean(outputSize, CV_32F, Scalar::all(0));
-    Mat outputImage;
+    Mat frame;
+    Size outputSize = getOutputSize(newFrameCap, compressionFactor);
+    Mat mean32F(outputSize, CV_32F, Scalar::all(0));
     Mat meanCV8U;
+    Mat outputImage;
     int frameCounter = 0;
     Timer t1;
-    ofstream f1("med_mean.txt"), f2("disp_mean.txt"), f3("med_filter.txt"), f4("disp_filter.txt"), f5("mean_filter.txt");
-
     while (true) {
-        Mat gray;
         frameCounter++;
         cout << "frameCounter = " << frameCounter << "\n";
 
         //t1.saveTimePoint();
         newFrameCap >> frame;
         //t1.saveTimePoint();
+        //t1.printLastDuration();
         if (frame.empty()) {
             return;
         }
         
         //t1.saveTimePoint();
-        resize(frame, frame, Size(), 1. / c, 1. / c);
-
+        resize(frame, frame, Size(), 1.0 / compressionFactor, 1.0 / compressionFactor);
         //t1.saveTimePoint();
-        //t1.printLastDuration();
-        cv::cvtColor(frame, gray, COLOR_BGR2GRAY);
-        gray.convertTo(gray, CV_32F, 1.0 / 255.0);
-        mean = mean + gray;
-        //t1.saveTimePoint();
-        //t1.printLastDuration();
         
-        int divider = (frameCounter % framesLimit == 0) ? framesLimit : (frameCounter % framesLimit);
-        mean.convertTo(meanCV8U, CV_8U, 255.0 * 1.0 / divider);
-        // write mean to file
-        //imwrite("E:\\Downloads\\dumps\\2lapl100\\m" + to_string(frameCounter) + ".jpg", meanCV8U);
-
-        
+        calculateMean(mean32F, meanCV8U, frame, frameCounter);
         detector->detect(meanCV8U, outputImage);
         //t1.saveTimePoint();
         //cout << "detector: ";
         //t1.printLastDuration();
-        v << outputImage;
-        if (frameCounter % framesLimit == 0) {
-            mean.setTo(0);
+
+        calculateMeanStandardDeviationMedian(meanCV8U, outputImage);
+
+        if (videoRecordingFlag) {
+            v << outputImage;
         }
 
-        //t1.saveTimePoint();
-        //t1.printFullDuration();
+        if (saveResultsFlag) {
+            saveResults(frame, meanCV8U, outputImage, frameCounter);
+        }
 
-        imshow("Mean frame", meanCV8U);
-        imshow("Filtered mean frame", outputImage);
-
-        //Mat m, disp;
-        //meanStdDev(meanCV8U, m, disp);
-        ////disp *= disp;
-        //int med = -1;
-        //std::vector<int> array;
-        //array.assign(meanCV8U.begin<uchar>(), meanCV8U.end<uchar>());
-        //std::nth_element(array.begin(), array.begin() + array.size() / 2, array.end());
-        //med = array[array.size() / 2];
-        //array.clear();
-
-        //f1 << med << "\n";
-        //f2 << disp.at<double>(0, 0) << "\n";
-
-        //meanStdDev(outputImage, m, disp);
-        ////disp *= disp;
-        //array.assign(outputImage.begin<uchar>(), outputImage.end<uchar>());
-        //std::nth_element(array.begin(), array.begin() + array.size() / 2, array.end());
-        //med = array[array.size() / 2];
-
-        //f3 << med << "\n";
-        //f4 << disp.at<double>(0, 0) << "\n";
-        //f5 << m.at<double>(0, 0) << "\n";
-
-        // write result to file
-        //imwrite("E:\\Downloads\\dumps\\2lapl100\\r" + to_string(frameCounter) + ".jpg", outputImage);
-        // write input frame to file
-        //imwrite("E:\\Downloads\\dumps\\1lapl100\\s" + to_string(frameCounter) + ".jpg", frame);
-
-        imshow("New frame", frame);
-        waitKey(1);
+        if (showResultsFlag) {
+            showResults(frame, meanCV8U, outputImage);
+        }
         t1.reset();
     }
 
     newFrameCap.release();
-    f1.close(); 
+    destroyAllWindows();
+}
+
+void VideoPlayer::initializeVideoWriter(VideoWriter& v, VideoCapture& cap, int compressionFactor) {
+    Size outputSize((int)cap.get(CAP_PROP_FRAME_WIDTH) / compressionFactor, (int)cap.get(CAP_PROP_FRAME_HEIGHT) / compressionFactor);
+    int ex = static_cast<int>(cap.get(CAP_PROP_FOURCC));
+    int fps = static_cast<int>(cap.get(CAP_PROP_FPS));
+    string name = "E:\\Downloads\\dumps\\xxxxx_" + to_string(compressionFactor) + ".avi";
+    v.open(name, ex, fps, outputSize, false);
+}
+
+cv::Size VideoPlayer::getOutputSize(VideoCapture& cap, int compressionFactor) {
+    Size outputSize((int)cap.get(CAP_PROP_FRAME_WIDTH) / compressionFactor, (int)cap.get(CAP_PROP_FRAME_HEIGHT) / compressionFactor);
+    return outputSize;
+}
+
+void VideoPlayer::calculateMean(cv::Mat& mean32F, cv::Mat& meanCV8U, const cv::Mat& inputFrame, int frameCounter) {
+    Mat gray;
+    cv::cvtColor(inputFrame, gray, COLOR_BGR2GRAY);
+    gray.convertTo(gray, CV_32F, 1.0 / 255.0);
+    mean32F = mean32F + gray;
+
+    int divider = (frameCounter % framesLimit == 0) ? framesLimit : (frameCounter % framesLimit);
+    mean32F.convertTo(meanCV8U, CV_8U, 255.0 * 1.0 / divider);
+    if (frameCounter % framesLimit == 0) {
+        mean32F.setTo(0);
+    }
+}
+
+void VideoPlayer::calculateMeanStandardDeviationMedian(const cv::Mat& mean, const cv::Mat& outputImage) {
+    ofstream f1("med_mean.txt", ios::app);
+    ofstream f2("disp_mean.txt", ios::app);
+    ofstream f3("med_filter.txt", ios::app);
+    ofstream f4("disp_filter.txt", ios::app);
+    ofstream f5("mean_filter.txt", ios::app);
+    Mat m, disp;
+    
+    meanStdDev(mean, m, disp);
+    // for variance instead of standard deviation use:
+    //disp *= disp;
+    int med = -1;
+    std::vector<int> array;
+    array.assign(mean.begin<uchar>(), mean.end<uchar>());
+    std::nth_element(array.begin(), array.begin() + array.size() / 2, array.end());
+    med = array[array.size() / 2];
+    array.clear();
+
+    f1 << med << "\n";
+    f2 << disp.at<double>(0, 0) << "\n";
+
+    meanStdDev(outputImage, m, disp);
+    // for variance instead of standard deviation use:
+    //disp *= disp;
+    array.assign(outputImage.begin<uchar>(), outputImage.end<uchar>());
+    std::nth_element(array.begin(), array.begin() + array.size() / 2, array.end());
+    med = array[array.size() / 2];
+
+    f3 << med << "\n";
+    f4 << disp.at<double>(0, 0) << "\n";
+    f5 << m.at<double>(0, 0) << "\n";
+    
+    f1.close();
     f2.close();
     f3.close();
     f4.close();
     f5.close();
-    destroyAllWindows();
+}
+
+void VideoPlayer::showResults(const cv::Mat& inputFrame, const cv::Mat& mean, const cv::Mat& outputImage) {
+    imshow("Mean frame", mean);
+    imshow("Filtered mean frame", outputImage);
+    imshow("New frame", inputFrame);
+    waitKey(1);
+}
+
+void VideoPlayer::saveResults(const cv::Mat& inputFrame, const cv::Mat& mean, const cv::Mat& outputImage, int frameCounter) {
+    // write input frame to file
+    imwrite("E:\\Downloads\\dumps\\2lapl100\\s" + to_string(frameCounter) + ".jpg", inputFrame);
+    // write mean to file
+    imwrite("E:\\Downloads\\dumps\\2lapl100\\m" + to_string(frameCounter) + ".jpg", mean);
+    // write result to file
+    imwrite("E:\\Downloads\\dumps\\2lapl100\\r" + to_string(frameCounter) + ".jpg", outputImage);
 }
--- overlapDetection/src/VideoPlayer.h
+++ overlapDetection/src/VideoPlayer.h
@@ -1,13 +1,34 @@
 #pragma once
+#include <opencv2/videoio.hpp>
+
 #include "EdgesDetector.h"
 
 class VideoPlayer {
-	private:
-		std::string pathToVideo;
-		EdgesDetector* detector;
-
 	public:
 		VideoPlayer(std::string& inputName, EdgesDetector* e) : pathToVideo(inputName), detector(e) {};
 
 		void playVideo();
+
+	private:
+		void initializeVideoWriter(cv::VideoWriter& v, cv::VideoCapture& cap, int compressionFactor);
+
+		cv::Size getOutputSize(cv::VideoCapture& cap, int compressionFactor);
+
+		void calculateMean(cv::Mat& mean, cv::Mat& meanCV8U, const cv::Mat& inputFrame, int frameCounter);
+
+		void calculateMeanStandardDeviationMedian(const cv::Mat& mean, const cv::Mat& outputImage);
+		
+		void saveResults(const cv::Mat& inputFrame, const cv::Mat& mean, const cv::Mat& outputImage, int frameCounter);
+		
+		void showResults(const cv::Mat& inputFrame, const cv::Mat& mean, const cv::Mat& outputImage);
+
+
+		const bool videoRecordingFlag = false;
+		const bool meanStandardDeviationMedianRecording = true;
+		const bool saveResultsFlag = false;
+		const bool showResultsFlag = true;
+		const int compressionFactor = 1;
+		const int framesLimit = 500;
+		std::string pathToVideo;
+		EdgesDetector* detector;
 };
