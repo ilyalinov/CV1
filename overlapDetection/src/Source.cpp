567f8604fada8e85c945249f52289892d1ab8c88
Ilya <ilya.lyalinov@gmail.com>
2021-04-04 21:13:45 +0300

    disable openCL

--- overlapDetection/src/Source.cpp
+++ overlapDetection/src/Source.cpp
@@ -1,9 +1,12 @@
 #include <iostream>
 
+#include <opencv2/core/ocl.hpp>
+
 #include "VideoHandler.h"
 #include "Configuration.h"
 
 int main(int argc, char** argv) {
+    cv::ocl::setUseOpenCL(false);
     Configuration c(argv[1]);
     VideoHandler v(&c);
     v.processVideo();
